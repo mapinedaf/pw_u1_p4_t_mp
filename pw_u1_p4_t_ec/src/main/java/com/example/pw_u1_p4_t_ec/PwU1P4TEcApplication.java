ef2722be8f3bc878ede95491b05ffe3e9821503c
mapinedaf <mapinedaf@uce.edu.ec>
2023-05-15 15:59:29 -0500

    creando repo

---
+++ pw_u1_p4_t_ec/src/main/java/com/example/pw_u1_p4_t_ec/PwU1P4TEcApplication.java
@@ -0,0 +1,13 @@
+package com.example.pw_u1_p4_t_ec;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PwU1P4TEcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PwU1P4TEcApplication.class, args);
+	}
+
+}
---
+++ pw_u1_p4_t_ec/src/test/java/com/example/pw_u1_p4_t_ec/PwU1P4TEcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pw_u1_p4_t_ec;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PwU1P4TEcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
